e81ce0edb0fb2f0938f6f28dd5259ce8c8a7ae87
akira <asa.shakira340@gmail.com>
2022-11-01 19:17:06 +0900

    feat: converts c to f and f to c

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The temperature to convert - must be a number
+    temperature: f64,
+
+    /// Celcius to Fahrenheit (default)
+    #[arg(short, long)]
+    celcius: bool,
+
+    /// Fahrenheit to Celcius
+    #[arg(short, long)]
+    fahrenheit: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    if args.fahrenheit {
+        let c = (args.temperature - 32.0) * 5.0 / 9.0;
+        println!("{} Celcius", c);
+    } else {
+        let f = args.temperature * 1.8 + 32.0;
+        println!("{} Fahrenheit", f);
+    }
 }
